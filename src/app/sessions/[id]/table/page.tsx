d350114c62bb4652938b6ad6a2cf397c98da9254
dnd721dev <dnd721dev@gmail.com>
2025-12-14 11:57:38 -0500

    update to react16.3

--- src/app/sessions/[id]/table/page.tsx
+++ src/app/sessions/[id]/table/page.tsx
@@ -1,10 +1,7 @@
 import { use } from 'react'
-import dynamic from 'next/dynamic'
-
-// Keep TableClient client-only (wallet/livekit/map stuff)
-const TableClient = dynamic(() => import('./TableClient'), { ssr: false })
+import TableClientShell from './TableClientShell'
 
 export default function TablePage(props: { params: Promise<{ id: string }> }) {
   const { id } = use(props.params)
-  return <TableClient sessionId={id} />
+  return <TableClientShell sessionId={id} />
 }
